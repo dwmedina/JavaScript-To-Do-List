5ca0e3da65e18b0cbb491e06f2c3f67e14e05451
Daniel Medina <11746348+dwmedina@users.noreply.github.com>
2018-08-29 10:59:26 -0400

    Begin building basic JS functions

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,5 +3,22 @@
 
 document.querySelector('#add').addEventListener('click',function() {
 var value = document.querySelector('#item').value;
+if (value) addItemTodo(value);
     
 });
+
+function addItemTodo(text) {
+    var item = document.createElement('li');
+    item.innerText = text;
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('buttons');
+
+    var remove = document.CreateElement('button');
+    remove.classList.add('remove');
+
+    var complete = document.createElement('button');
+    complete.classList.add('complete');
+
+
+}
