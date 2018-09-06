4b4e042fa810f5641ec7500bf6c2003f9cc93ad6
gurpreet cheema <34461294+GurpreetCheema@users.noreply.github.com>
2018-09-06 11:46:46 -0400

    pushing

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,7 +8,8 @@ var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xli
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect y="0" class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>';
 
 renderTodoList();
-
+//user clciked on the add button
+//if there is any text isnide the item field add that text to the todo list
 document.querySelector('#add').addEventListener('click', function() {
 var value = document.querySelector('#item').value;
 if (value) {
@@ -22,6 +23,14 @@ document.querySelector('#item').addEventListener('keydown', function(e) {
         addItem(value);
     }
 });
+
+fucntion addItem (value) {
+    addItemToDOM(value);
+    document.querySelector('#item").value = '';
+                           
+data.todo.push(value);
+    dataObjectUpdated();
+}
 
 function renderTodoList() {
     if (!data.todo.length && !data.completed.length) return;
@@ -37,10 +46,23 @@ function renderTodoList() {
     }
 }
 
+fucntion dataObjectUpdated() {
+    localStorage.setItem('todoList',JSON.stringify(data));}
+
 function removeItem(e) {
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
-
+var id = parent.id;
+    var value = item.innerText;
+    
+    
+    if (id ==='todo' ){
+        data.todo.splice(data.todo.indexOf(value),1);
+    }else {
+        
+    data.completed.splice(data.completed.indexOf(value),1);
+    }
+    dataObjectUpdted();
     parent.removeChild(item);
 }
 
