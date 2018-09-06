55fb1d895686184978e03a11e72a491e18bb01eb
Daniel Medina <11746348+dwmedina@users.noreply.github.com>
2018-09-06 12:00:55 -0400

    CompleteItem function finished

--- resources/js/main.js
+++ resources/js/main.js
@@ -69,6 +69,28 @@ function removeItem() {
     parent.removeChild(item);
 }
 
+function completeItem() {
+    var item - this.parentNode.parentNode;
+    var parent = item.parentNode;
+    var id = parent.id;
+    var value = item.innerText;
+
+    if (id === 'todo') {
+        data.todo.splice(data.todo.indexOf(value), 1);
+        data.completed.push(value);
+    } else {
+        data.completed.splice(data.completed.indexOf(value), 1);
+        data.todo.push(value);
+    }
+    dataObjectUpdated();
+
+    // Check if the item should be added to the completed list or re-added to the todo list
+    var target = (id === 'todo') ? document.querySelector('#completed') : document.querySelector('#todo');
+
+    parent.removeChild(item);
+    target.insertBefore(item, target.childNodes[0]);
+}
+
 // Adds a new item to the todo list
 function addItemToDOM (text, completed) {
     var list = (completed) ? document.querySelector('#completed') : document.querySelector('#todo');
